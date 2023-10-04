090eec336fcc2f01cca48fc08823ab45168aa24d
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-04 12:11:31 +0530

    Project completed

--- src/Components/ViewProducts.jsx
+++ src/Components/ViewProducts.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Header from './Header'
 
 const ViewProducts = () => {
     const [productData,setProductData]=useState(
@@ -21,6 +22,7 @@ const ViewProducts = () => {
     useEffect(()=>{getData()},[])
   return (
     <div>
+        <Header/>
         <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
