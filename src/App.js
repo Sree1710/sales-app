cbb6f42d4a6852c251a2358000756ae10dbe6627
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-04 11:00:18 +0530

    Design Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddProducts from './Components/AddProducts';
 
 function App() {
   return (
     <div>
-
+    <AddProducts/>
     </div>
   );
 }
