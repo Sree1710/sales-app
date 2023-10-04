1f912af2a1c48d98899b5ce1c3ee2435b75e99ec
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-04 12:05:51 +0530

    Routing Implemented

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import AddProducts from './Components/AddProducts';
 import SearchProducts from './Components/SearchProducts';
 import ViewProducts from './Components/ViewProducts';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-    
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" exact element={<AddProducts/>} />
+    <Route path="/s" exact element={<SearchProducts/>} />
+    <Route path="/v" exact element={<ViewProducts/>} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
